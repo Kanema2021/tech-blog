4c55f5d04c3e9f27261ff8d594e99723aee65fe9
Karen Marting <karenannemarting@yahoo.com>
2021-09-01 17:33:44 -0700

    routes and models added

---
+++ models/Blog.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Blog extends Model { }
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    blog_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    visitor_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'blog',
+  }
+);
+
+module.exports = Blog;
